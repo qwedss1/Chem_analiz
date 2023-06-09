3b4eb78139a69c325df162b987116962475c507f
qwedss1 <131647247+qwedss1@users.noreply.github.com>
2023-06-09 20:02:29 +0300

    danya

--- src/dict_formation.py
+++ src/dict_formation.py
@@ -1,6 +1,21 @@
-def dict_formation(d:dict):
-    '''
-    dict={"A":["name",moles,"state"],"B":["name",moles,"state"],...}
-    Функция вернет строку с форматированием + забьет моли если они нули + вернет 2 листа вида ['name',moles,state] [...] где листы разделены по принципу A+B=C+D-> A+B->list1 C+D->list2
-    '''
-    
+import json
+import os
+def dict_formation():
+    d=dict()
+    d1=dict()
+    d2=dict()
+    with open('cashew.json') as f:
+        d=json.load(f)
+    k=0
+    for key,value in d.items():
+        if k<2 && value!=[]:
+            d1[key]=value
+        elif value!=[]:
+            d2[key]=value
+        k++
+    d=dict()
+    d.update(d1)
+    d.update(d2)
+    os.remove('cashew.json')
+    with open('cashew.json') as f:
+        d=json.dump(d,f)              
