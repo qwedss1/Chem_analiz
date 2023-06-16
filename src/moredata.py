3bb8cd6ea37c9f304e68d257d8d6473addf36f01
qwedss1 <qwedss2013@yandex.ru>
2023-06-16 09:34:48 +0300

    Даня

--- src/moredata.py
+++ src/moredata.py
@@ -1,6 +1,83 @@
 from tkinter import *
 import tkinter.font as tkFont
+import json
+
 
 class Moredata:
     def __init__(self,a,num):
         self.Init=a
+        self.pathC=f"Reactions/Reaction{num}/Calcu.json"
+        self.pathR = f"Reactions/Reaction{num}/React.json"
+        self.make_root()
+        self.run()
+
+    def make_root(self):
+        self.root = Tk()
+        root = self.root
+        w,h=520,500
+        root.geometry(f"{w}x{h}")
+        root.minsize(w, h)
+        root.maxsize(w, h)
+        root.config(bg="gray")
+        root.title("Дополнительные рассчеты")
+        self.make_menu(root)
+
+    def make_menu(self, root):
+        fs=20
+        if self.is_state() == "L":
+            Label(root, text=f"Ваша реакция происходит с жидкостями", font=tkFont.Font(size=fs-2), justify="center").pack(fill=X)
+            Label(root, text=f"можно рассчитать:",font=tkFont.Font(size=fs), justify="center").pack(fill=X)
+            Label(root,bg="gray").pack()
+            Button(root, text="Энергия активации", font=tkFont.Font(size=fs)).pack(fill=X)
+            Button(root, text="Равновесные концентрации", font=tkFont.Font(size=fs)).pack(fill=X)
+            Button(root, text="Константа равновесия", font=tkFont.Font(size=fs)).pack(fill=X)
+        if self.is_state() == "G":
+            Label(root, text=f"Ваша реакция происходит с газами",font=tkFont.Font(size=fs), justify="center").pack(fill=X)
+            Label(root, text=f"можно рассчитать:",font=tkFont.Font(size=fs), justify="center").pack(fill=X)
+            Label(root, bg="gray").pack()
+            Button(root, text="Равновесные мольные доли", font=tkFont.Font(size=fs)).pack(fill=X)
+            Button(root, text="Константа равновесия", font=tkFont.Font(size=fs)).pack(fill=X)
+        Button(root, text="График LnK(T)", font=tkFont.Font(size=fs)).pack(fill=X)
+
+    #Диффренцируем реакции на газово-солидные и ликвидно-солидные
+    def is_state(self):
+        compounds=[]
+        with open(self.pathR,"r") as f:
+            for x in json.load(f)["R"]:
+                compounds.append(x[1])
+        with open(self.pathR, "r") as f:
+            for x in json.load(f)["P"]:
+                compounds.append(x[1])
+        flag = False
+        for x in compounds:
+            if x=="Solid":
+                flag=True
+            else:
+                flag=False
+                break
+        if flag:
+            return None
+
+        flag = False
+        for x in compounds:
+            if x=="Liquid" or x=="Solid":
+                flag=True
+            else:
+                flag=False
+                break
+        if flag:
+            return "L"
+
+        for x in compounds:
+            if x=="Gas" or x=="Solid":
+                flag=True
+            else:
+                flag=False
+                break
+        if flag:
+            return "G"
+        else:
+            return None
+
+    def run(self):
+        self.root.mainloop()
