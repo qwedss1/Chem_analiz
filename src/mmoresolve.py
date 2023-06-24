6d7dc95d99db7a5088ec53cfd2a950f978c0e9ca
apcen <apcen221@gmail.com>
2023-06-24 19:38:30 +0300

    senya

--- src/mmoresolve.py
+++ src/mmoresolve.py
@@ -16,43 +16,66 @@ class ms:
     def EqMolLStech(num):
         A = solvechem(num)
         z = Symbol('z')
+        reag = A.reag
+        prod = A.prod
+        coefs = A.coefs
+        for n in range(0, len(reag)):
+            if reag[n][1] == "Solid":
+                del reag[n]
+                del coefs[0][n]
+        for n in range(0, len(prod)):
+            if prod[n][1] == "Solid":
+                del prod[n]
+                del coefs[1][n]
         c = 1
         ca = 0
-        for n in range(0,len(A.reag)):
-            c = c / (A.coefs[0][n]**A.coefs[0][n])
-        for n in range(0,len(A.prod)):
-            c = c * (A.coefs[1][n]**A.coefs[1][n])
-        for n in range(0, len(A.reag)):
-            ca = ca - A.coefs[0][n]
-        for n in range(0, len(A.prod)):
-            ca = ca + A.coefs[1][n]
+        for n in range(0, len(reag)):
+            c = c / (coefs[0][n] ** coefs[0][n])
+        for n in range(0, len(prod)):
+            c = c * (coefs[1][n] ** coefs[1][n])
+        for n in range(0, len(reag)):
+            ca = ca - coefs[0][n]
+        for n in range(0, len(prod)):
+            ca = ca + coefs[1][n]
         eqn = Eq(c*(1-z)**ca,ms.Kravn(num))
-        b = ms.returnz(solve(eqn,z)[0],A)
+        b = ms.returnz(solve(eqn,z)[0],num)
         return b
     @staticmethod
     def EqMolGStech(num,P):
         A = solvechem(num)
+        reag = A.reag
+        prod = A.prod
+        coefs = A.coefs
+        for n in range(0, len(reag)):
+            if reag[n][1] == "Solid":
+                del reag[n]
+                del coefs[0][n]
+        for n in range(0, len(prod)):
+            if prod[n][1] == "Solid":
+                del prod[n]
+                del coefs[1][n]
         all = 0
         z = Symbol('z')
-        for n in range(0, len(A.reag)):
-            all = all + A.coefs[0][n]
-        for n in range(0, len(A.prod)):
-            all = all + A.coefs[1][n]
+        for n in range(0, len(reag)):
+            all = all + coefs[0][n]
+        for n in range(0, len(prod)):
+            all = all + coefs[1][n]
         c = 1
         ca = 0
-        for n in range(0, len(A.reag)):
-            c = c / (A.coefs[0][n] ** A.coefs[0][n])
-        for n in range(0, len(A.prod)):
-            c = c * (A.coefs[1][n] ** A.coefs[1][n])
-        for n in range(0, len(A.reag)):
-            ca = ca - A.coefs[0][n]
-        for n in range(0, len(A.prod)):
-            ca = ca + A.coefs[1][n]
+        for n in range(0, len(reag)):
+            c = c / (coefs[0][n] ** coefs[0][n])
+        for n in range(0, len(prod)):
+            c = c * (coefs[1][n] ** coefs[1][n])
+        for n in range(0, len(reag)):
+            ca = ca - coefs[0][n]
+        for n in range(0, len(prod)):
+            ca = ca + coefs[1][n]
         eq = Eq(c*(P*(1-z)*(all+ca*z))**ca,ms.Kravn(num))
-        b = ms.returnz(solve(eq, z)[0], A)
+        b = ms.returnz(solve(eq, z)[0], num)
         return b
     @staticmethod
-    def returnz(z,A):
+    def returnz(z,num):
+        A = solvechem(num)
         a = [z,[]]
         for n in range(0, len(A.reag)):
             a[1].append((A.reag[n][0],A.coefs[0][n]-z))
